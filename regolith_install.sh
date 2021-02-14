e37badced77ad38904117eb5b40a4f827fcbd4a0
Koen Gabriels <koen.gabriels@gmail.com>
2021-02-14 17:45:31 +0100

    moves regolith install to separate shell script

---
+++ regolith_install.sh
@@ -0,0 +1,17 @@
+################################################################################################################################
+####						Installing Regolith								                                                                         ####
+###############################################################################################################################
+
+sudo add-apt-repository -y ppa:regolith-linux/release
+sudo apt install -y regolith-desktop-mobile
+
+## install different looks for Regolith
+sudo apt-install -y regolith-look-ayu
+sudo apt-install -y regolith-look-ayu-dark
+sudo apt-install -y regolith-look-ayu-mirage
+sudo apt-install -y regolith-look-cahuella
+sudo apt-install -y regolith-look-dracula
+sudo apt-install -y regolith-look-gruvbox
+sudo apt-install -y regolith-look-nord
+sudo apt-install -y regolith-look-solarized-dark
+sudo apt-install -y regolith-look-ubuntu
