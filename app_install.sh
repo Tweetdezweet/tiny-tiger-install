ee2ea2387ecf28223d57f36ba335296c95bbe163
Koen Gabriels <koen.gabriels@gmail.com>
2021-02-14 18:05:40 +0100

    moves slack install to separate script

--- app_install.sh
+++ app_install.sh
@@ -11,13 +11,6 @@ sudo apt-get install -y darktable
 ###############################################################################################################################
 sudo apt-get install -y git
 
-
-################################################################################################################################
-####					Installing Slack as a Snap package								                                                           ####
-###############################################################################################################################
-sudo snap install slack --classic
-
-
 ################################################################################################################################
 ####					Installing Filezilla with apt								                                                                  ####
 ###############################################################################################################################
@@ -62,26 +55,6 @@ sudo snap install phpstorm-classic
 ## get file from: https://cdn.localwp.com/releases-stable/5.9.8+5191/local-5.9.8-linux.deb
 
 wget -qO - https://cdn.localwp.com/releases-stable/5.9.8+5191/local-5.9.8-linux.deb | sudo apt install -y -
-
-
-################################################################################################################################
-####						Installing Regolith								                                                                         ####
-###############################################################################################################################
-
-sudo add-apt-repository -y ppa:regolith-linux/release
-sudo apt install -y regolith-desktop-mobile
-
-## install different looks for Regolith
-sudo apt-install -y regolith-look-ayu
-sudo apt-install -y regolith-look-ayu-dark
-sudo apt-install -y regolith-look-ayu-mirage
-sudo apt-install -y regolith-look-cahuella
-sudo apt-install -y regolith-look-dracula
-sudo apt-install -y regolith-look-gruvbox
-sudo apt-install -y regolith-look-nord
-sudo apt-install -y regolith-look-solarized-dark
-sudo apt-install -y regolith-look-ubuntu
-
 
 ################################################################################################################################
 ####					Installing Chrome and it's extensions			                                                             				####
