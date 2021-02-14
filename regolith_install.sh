833a691facbfb4c811d84e9029328734cbd9733e
Koen Gabriels <koen.gabriels@gmail.com>
2021-02-14 17:49:42 +0100

    fixes typos in regolith.sh

--- regolith_install.sh
+++ regolith_install.sh
@@ -6,12 +6,12 @@ sudo add-apt-repository -y ppa:regolith-linux/release
 sudo apt install -y regolith-desktop-mobile
 
 ## install different looks for Regolith
-sudo apt-install -y regolith-look-ayu
-sudo apt-install -y regolith-look-ayu-dark
-sudo apt-install -y regolith-look-ayu-mirage
-sudo apt-install -y regolith-look-cahuella
-sudo apt-install -y regolith-look-dracula
-sudo apt-install -y regolith-look-gruvbox
-sudo apt-install -y regolith-look-nord
-sudo apt-install -y regolith-look-solarized-dark
-sudo apt-install -y regolith-look-ubuntu
+sudo apt-get install -y regolith-look-ayu
+sudo apt-get install -y regolith-look-ayu-dark
+sudo apt-get install -y regolith-look-ayu-mirage
+sudo apt-get install -y regolith-look-cahuella
+sudo apt-get install -y regolith-look-dracula
+sudo apt-get install -y regolith-look-gruvbox
+sudo apt-get install -y regolith-look-nord
+sudo apt-get install -y regolith-look-solarized-dark
+sudo apt-get install -y regolith-look-ubuntu
